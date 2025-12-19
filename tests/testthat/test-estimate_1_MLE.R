3123fe971816afa967d615c59b35c17a5ff19330
yuki <hmz1969a@gmail.com>
2025-12-19 18:23:43 +0800

    v0.1.8
    笔误

--- tests/testthat/test-estimate_1_MLE.R
+++ tests/testthat/test-estimate_1_MLE.R
@@ -34,7 +34,7 @@ testthat::test_that("MLE", {
     ),
     settings = list(name = c("TD", "RSTD", "Utility"), policy = "off"),
     # 算法
-    algorithm = algorithm = "NLOPT_GN_MLSL",
+    algorithm = "NLOPT_GN_MLSL",
     lowers = list(c(0, 0), c(0, 0, 0), c(0, 0, 0)),
     uppers = list(c(1, 1), c(1, 1, 1), c(1, 1, 1)),
     control = list(core = 4, iter = 5)
